6dd333cd0c9b26183bcce7ec165ca6ff553f9099
sahilbansal123-lang <bansalsahil9052@gmail.com>
2024-01-09 12:04:54 +0530

    sahil

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Staking is ReentrancyGuard {
+    using SafeMath for uint256;
     IERC20 public s_stakeingToken;
     IERC20 public s_rewardToken;
 
@@ -14,6 +15,33 @@ contract Staking is ReentrancyGuard {
     uint public rewardPerTokenStored;
     uint public lastUpdateTime;
 
-    
+    mapping (address=>uint256) public stakedBalance;
+    mapping (address=>uint256) public rewards;
+    mapping (address=>uint256) public userRewardPerTokenPaid;
+
+    event Staked(address indexed user, uint256 indexed amount);
+    event Withdrawn(address indexed user, uint256 indexed amount);
+    event RewardsClaimed(address indexed user, uint256 indexed amount);
+
+    constructor(address stakingToken, address rewardToken) {
+        s_stakeingToken = IERC20(stakingToken);
+        s_rewardToken = IERC20(rewardToken);
+    }
+
+    function rewardPerToken() public view returns(uint) {
+        if(totalStakedTokens == 0) {
+            return rewardPerTokenStored;
+        }
+
+        uint totalTime = block.timestamp-lastUpdateTime;
+        uint totalRewards = REWARD_RATE*totalTime;
+        return rewardPerTokenStored + totalRewards/totalStakedTokens;
+    }
+
+    function earned(address account) public view returns(uint) {
+        return(stakedBalance[account]*(rewardPerToken()-userRewardPerTokenPaid[account]))
+    }
+
+
 }
 
